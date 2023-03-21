1d23c76607b666867459be5198988a5412d8dfbd
whi_co <113486696+sungjun0629@users.noreply.github.com>
2023-03-21 18:29:12 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int arr[9];
+
+int main()
+{
+	for (int i = 0; i < 9; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + 9);
+
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) sum += arr[i];
+		if (sum == 100) break;
+	} while (next_permutation(arr, arr + 9));
+
+	for (int i = 0; i < 7; i++) cout << arr[i]<<"\n";
+	return 0;
+
+}
