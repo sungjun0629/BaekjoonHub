dca6dfdadb485e934988c5c79b433a0767ea2a78
whi_co <113486696+sungjun0629@users.noreply.github.com>
2024-01-16 17:13:53 +0900

    [Gold III] Title: 순회강연, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,45 +1,44 @@
 #include<iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
 using namespace std;
 
-int n,d,p; 
-vector<pair<int, int>> vec;
-priority_queue<int , vector<int>, greater<int>> pq; 
-int ret;
 
+int n, p, d;
+vector<pair<int, int>> vec;
+int day, ret;
+// 오름차순 진행, 루트노드가 제일 작다. 
+priority_queue<int, vector<int>, greater<int>> pq;
 int main()
 {
-	cin>>n; 
+	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> p >> d; 
-		// day를 기준으로 정렬할 것이므로 day를 가장 먼저 넣어준다. 
-		vec.push_back({d, p});
+		cin >> p >> d;
+		vec.push_back({ d,p });
 	}
 
-	// day를 기준으로 오름차순을 진행한다. 
+	// d일 안에 와서 강연을 해주면 p만큼의 강연료를 지불한다.
+	// d를 기준으로 오름차순을 진행하고 
 	sort(vec.begin(), vec.end());
-
-	// priority_queue를 이용하여, pay가 가장 작은 것을 빼준다. 
-	for (pair<int, int> e : vec)
+	for (int i = 0; i < vec.size(); i++)
 	{
-		pq.push(e.second);
-		//cout<< e.second << "를 넣어줍니다." << endl;
-		ret += e.second; 
-		if (pq.size() > e.first)
-		{// 만약, 지금 넣고 있는 pay에 대한 day가 pq 사이즈보다 작다면
-		 // pq에서 가장 작은 것을 하나 빼준다. 
-			ret -= pq.top();
-			//cout<< "사이즈를 초과하여 " << pq.top() << "을 빼줍니다." <<endl;
-			pq.pop();
+
+		if (pq.size() < vec[i].first)
+		{
+			pq.push(vec[i].second);
+			ret += vec[i].second;
+		}
+		// d를 기준으로 강연을 갈 수 있는지 판단을 한다.
+		else if (pq.size() >= vec[i].first && pq.top() < vec[i].second)
+		{
+			ret -= pq.top(); pq.pop();
+			ret += vec[i].second; pq.push(vec[i].second);
 		}
 	}
-	// 위의 for문을 모두 순회하면
-	// day일정에 맞게 최대한으로 큰 pay를 가져온 결과를 확인할 수 있다. 
-	
-	cout<< ret;
 
+	cout << ret;
 
 }
