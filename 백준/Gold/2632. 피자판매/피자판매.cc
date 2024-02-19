84773fdb796ac39a4751a08ab045c6bece355fa3
whi_co <113486696+sungjun0629@users.noreply.github.com>
2024-02-19 09:35:59 +0900

    [Gold II] Title: 피자판매, Time: 488 ms, Memory: 66332 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.cc
+++ 백준/Gold/2632. 피자판매/피자판매.cc
@@ -1,38 +1,78 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include<iostream>
 #include<map>
 
-using namespace std;
-int a[1001], b[1001], ret, psum_a[2002], psum_b[2002], want, n, m;
-map<int, int> aCnt, bCnt;
 
-void make(int n, int psum[], map<int, int>& mp) {
-	for (int interval = 1; interval <= n; interval++) {
-		for (int start = interval; start <= n + interval - 1; start++) {
-			int sum = psum[start] - psum[start - interval];
-			mp[sum]++;
+using namespace std;
+
+int k, m, n;
+int a_p[1004], b_p[1004];
+// 원형이므로 기존의 조각보다 2배를 해준다.
+int sum_a[2010], sum_b[2010];
+map<int, int> a_cnt, b_cnt;
+
+void sum_pizza(int n, map<int, int>& mm, int sum[])
+{
+	for (int interval = 1; interval<=n ; interval++)
+	{
+		// n번만 수행을 한다. 
+		for (int start = interval; start <= n + interval - 1; start++)
+		{
+			int sum_ = sum[start] - sum[start - interval];
+			mm[sum_]++;
+
 			if (interval == n) break;
 		}
-	}
+	};
 }
-int main() {
-	scanf("%d %d %d", &want, &n, &m);
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", a + i);
-		psum_a[i] = psum_a[i - 1] + a[i];
+
+
+int main()
+{
+	cin >> k >> m >> n;
+	// 피자 조각 크기
+	// 합 배열을 위해 1부터 시작한다. 
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> a_p[i];
+		// 누적 합  
+		sum_a[i] = sum_a[i - 1] + a_p[i];
 	}
-	for (int i = n + 1; i <= 2 * n; i++) psum_a[i] = psum_a[i - 1] + a[i - n];
-	for (int i = 1; i <= m; i++) {
-		scanf("%d", b + i);
-		psum_b[i] = psum_b[i - 1] + b[i];
+
+	// 원형 순환 계산을 위해 2배로 늘린다. 
+	for (int i = m + 1; i <= 2 * m; i++)
+	{
+		sum_a[i] = sum_a[i - 1] + a_p[i - m];
 	}
-	for (int i = m + 1; i <= 2 * m; i++)psum_b[i] = psum_b[i - 1] + b[i - m];
-	make(n, psum_a, aCnt);
-	make(m, psum_b, bCnt);
-	ret = (aCnt[want] + bCnt[want]);
-	for (int i = 1; i < want; i++) {
-		ret += (aCnt[i] * bCnt[want - i]);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> b_p[i];
+		sum_b[i] = sum_b[i - 1] + b_p[i];
 	}
-	printf("%d\n", ret);
+
+	for (int i = n + 1; i <= 2 * n; i++)
+	{
+		sum_b[i] = sum_b[i - 1] + b_p[i - n];
+	}
+
+	// 피자 합 배열로부터 모든 구간 합을 구하며 
+	// 가능한 피자의 합에 대한 개수를 구한다. 
+	sum_pizza(m, a_cnt, sum_a);
+	sum_pizza(n, b_cnt, sum_b);
+
+	int ret = 0;
+	// 각각 구한 가능한 피자 합으로부터 
+	// 경우의 수를 구한다. 
+	for (int i = 1; i < k; i++)
+	{
+		int a = a_cnt[i];
+		int b = b_cnt[k - i];
+
+		ret += a * b;
+	}
+
+	ret += a_cnt[k] + b_cnt[k];
+
+	cout << ret;
+
 }
